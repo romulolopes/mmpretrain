8552711ffbf9bc47f281cfd89b4feee649c8569e
Rômulo Lopes <romulo.lopes@ifce.edu.br>
2024-10-27 20:27:24 -0300

    Update cariotipo.py

--- configs/hivit/cariotipo.py
+++ configs/hivit/cariotipo.py
@@ -2,7 +2,7 @@
 #modificado dataset imagenet_bs64_hivit_224
 _base_ = [
     '../_base_/models/hivit/tiny_224.py',
-    '../_base_/datasets/imagenet_bs64_hivit_224.py',
+    '../_base_/datasets/cariotipo.py',
     '../_base_/schedules/imagenet_bs1024_adamw_hivit.py',
     '../_base_/default_runtime.py'
 ]
@@ -10,3 +10,11 @@ _base_ = [
 # schedule settings
 optim_wrapper = dict(clip_grad=dict(max_norm=5.0))
 
+mlflow_tags = {
+        "model_type": "Hivit",
+        "dataset": "Cariotipo",
+}
+
+custom_hooks = [
+    dict(type='MlflowLoggerHook',  exp_name="Hivit" , params=mlflow_tags)
+]
