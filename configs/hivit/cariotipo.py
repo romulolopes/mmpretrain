730f89bf67c74554721e967d6a8c66fc2220ce50
Rômulo Lopes <romulo.lopes@ifce.edu.br>
2024-10-27 20:33:25 -0300

    Update cariotipo.py

--- configs/hivit/cariotipo.py
+++ configs/hivit/cariotipo.py
@@ -1,14 +1,91 @@
 # hivit-tiny-p16_16xb64_in1k
 #modificado dataset imagenet_bs64_hivit_224
 _base_ = [
-    '../_base_/models/hivit/tiny_224.py',
-    '../_base_/datasets/cariotipo.py',
-    '../_base_/schedules/imagenet_bs1024_adamw_hivit.py',
-    '../_base_/default_runtime.py'
+    './hivit-tiny-p16_16xb64_in1k.py'
 ]
 
-# schedule settings
-optim_wrapper = dict(clip_grad=dict(max_norm=5.0))
+
+num_classes = 2
+data_preprocessor = dict(
+    num_classes=num_classes)
+
+model = dict(
+    head=dict(
+        num_classes=num_classes,
+    ))
+
+warmup_epochs = 10
+base_lr = 5e-4
+
+optim_wrapper = dict(
+    _delete_=True,
+    type='AmpOptimWrapper',
+    optimizer=dict(type='AdamW', lr=base_lr, weight_decay=0.001),
+    # specific to vit pretrain
+    paramwise_cfg=dict(custom_keys={
+        '.cls_token': dict(decay_mult=0.0),
+        '.pos_embed': dict(decay_mult=0.0)
+    }),
+)
+
+param_scheduler = [
+    # warm up learning rate scheduler
+    dict(
+        type='LinearLR',
+        start_factor=1e-4,
+        by_epoch=True,
+        end=warmup_epochs,
+        # update by iter
+        convert_to_iter_based=True),
+    # main learning rate scheduler
+    dict(
+        type='CosineAnnealingLR',
+        eta_min=1e-5,
+        by_epoch=True,
+        begin=warmup_epochs)
+]
+
+
+data_root = 'data/cariotipo/'
+train_image_folder = "train"
+val_image_folder = "test" 
+IMAGENET_CATEGORIES = ["alterado", "normal"]
+METAINFO = {'classes': IMAGENET_CATEGORIES}
+
+train_dataloader = dict(
+    dataset=dict(
+        metainfo=METAINFO,
+        data_root=data_root,
+        data_prefix=train_image_folder
+        )
+)
+
+val_dataloader = dict(
+    dataset=dict(
+        metainfo=METAINFO,
+        data_root=data_root,
+        data_prefix=val_image_folder
+        )
+)
+
+val_evaluator = dict(type='Accuracy', topk=(1, ))
+test_evaluator = val_evaluator
+
+
+max_epochs = 30
+train_cfg = dict(by_epoch=True, max_epochs=max_epochs, val_interval=1)
+val_cfg = dict()
+test_cfg = dict()
+
+# local path to saving the models and logs
+work_dir = "./out"
+
+# configure default hooks
+default_hooks = dict(
+    # save checkpoint per epoch.
+    checkpoint=dict(type='CheckpointHook', max_keep_ckpts=1),
+
+)
 
 mlflow_tags = {
         "model_type": "Hivit",
