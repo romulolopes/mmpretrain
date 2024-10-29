24a02f4541bcb4fa7dadc3d2c3503f507dadb4ed
Rômulo Lopes <romulo.lopes@ifce.edu.br>
2024-10-29 19:34:23 -0300

    Update cariotipo.py

--- configs/hrnet/cariotipo.py
+++ configs/hrnet/cariotipo.py
@@ -1,5 +1,5 @@
 _base_ = [
-    './hrnet-w18_3rdparty_8xb32_in1k.py'
+    './hrnet-w18_8xb32_in1k.py'
 ]
 
 load_from = "https://download.openmmlab.com/mmclassification/v0/hrnet/hrnet-w18_3rdparty_8xb32_in1k_20220120-0c10b180.pth"
