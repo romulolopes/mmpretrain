1071b4f20ade63fab36bef5de33a3b0f1532d669
Rômulo Lopes <romulo.lopes@ifce.edu.br>
2024-10-29 13:53:11 -0300

    Create cariotipo.py

---
+++ configs/hrnet/cariotipo.py
@@ -0,0 +1,14 @@
+_base_ = [
+    './hrnet-w18_3rdparty_8xb32_in1k.py'
+]
+
+load_from = "https://download.openmmlab.com/mmclassification/v0/hrnet/hrnet-w18_3rdparty_8xb32_in1k_20220120-0c10b180.pth"
+
+mlflow_tags = {
+        "model_type": "HRNet",
+        "dataset": "Cariotipo",
+}
+
+custom_hooks = [
+    dict(type='MlflowLoggerHook',  exp_name="HRNet" , params=mlflow_tags)
+]
